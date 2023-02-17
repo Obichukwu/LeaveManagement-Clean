5b2b54d3b47663f17db867e89b42c0832568a062
Obichukwu Onyeowuzoni <obichukwuonyeowuzoni@chevron.com>
2023-02-17 16:53:44 +0100

    Add project files.

---
+++ HR.LeaveManagement.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,13 @@
+﻿using HR.LeaveManagement.Application.Contracts.Persistence;
+using HR.LeaveManagement.Domain.Common;
+
+namespace HR.LeaveManagement.Application.Contracts.Persistence;
+
+public interface IGenericRepository<T> where T : BaseEntity
+{
+    Task<T> GetAsync();
+    Task<T> GetByIdAsync(int id);
+    Task<T> CreateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
